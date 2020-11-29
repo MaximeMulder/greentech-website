c593f77d6bbbd96efbdd55037ee509a9b70acef3
Maxime Mulder <maxime.mulder@viacesi.fr>
2020-11-29 12:39:10 +0100

    use type inference in Live.tsx

--- src/Live.tsx
+++ src/Live.tsx
@@ -10,12 +10,12 @@ function Live(): ReactElement {
   const live = getLive(Number(useParams<LiveParams>().live));
   const time_until_live = getLiveConferences(live)[0].begin - Math.floor(Date.now() / 1000);
   console.log(time_until_live);
-  const days :number = Math.floor(time_until_live / 86400);
-  let remainder :number = time_until_live % 86400;
-  const hours :number = Math.floor(remainder / 3600);
+  const days = Math.floor(time_until_live / 86400);
+  let remainder = time_until_live % 86400;
+  const hours = Math.floor(remainder / 3600);
   remainder = remainder % 3600;
-  const minutes :number = Math.floor(remainder / 60);
-  const seconds: number = remainder % 60;
+  const minutes = Math.floor(remainder / 60);
+  const seconds = remainder % 60;
   return (
     <React.Fragment>
       <iframe title={live.name} src={live.video} width="720" height="480"></iframe>
