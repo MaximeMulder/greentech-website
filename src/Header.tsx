5d849fcdd4d3cd8952122dabd76aad82a55c84c3
Nenrikido <ampy00@live.fr>
2020-11-29 16:51:24 +0100

    fix responsive header menu to be usable on phone

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,24 +1,38 @@
-import React, { ReactElement } from 'react';
+import React, { Component, ReactElement } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.scss';
 
-const Header = (): ReactElement => (
-  <header>
-    <img src="greentech-logo.png" alt="Logo GreenTech" />
-    <div className="svg">
-      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="34px" height="27px" viewBox="0 0 34 27" enableBackground="new 0 0 34 27">
-        <rect fill="#FFFFFF" width="34" height="4"/>
-        <rect y="11" fill="#FFFFFF" width="34" height="4"/>
-        <rect y="23" fill="#FFFFFF" width="34" height="4"/>
-      </svg>
-    </div>
-    <nav>
-      <Link to="/">Présentation</Link>
-      <Link to="/conferences">Conférences</Link>
-      <Link to="/planning">Plannification</Link>
-      <Link to="/lives">Diffusions</Link>
-    </nav>
-  </header>
-);
+class Header extends Component {
+  constructor(props: never) {
+    super(props);
+
+    this.burgerToggle = this.burgerToggle.bind(this);
+  }
+
+  burgerToggle() {
+    document.getElementById('burgerToggle').classList.toggle('pressed');
+  }
+
+  render(): ReactElement {
+    return (
+      <header>
+        <img src="greentech-logo.png" alt="Logo GreenTech" />
+        <div id="burgerToggle" onClick={this.burgerToggle}>
+          <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="34px" height="27px" viewBox="0 0 34 27" enableBackground="new 0 0 34 27">
+            <rect fill="#FFFFFF" width="34" height="4"/>
+            <rect y="11" fill="#FFFFFF" width="34" height="4"/>
+            <rect y="23" fill="#FFFFFF" width="34" height="4"/>
+          </svg>
+        </div>
+        <nav onClick={this.burgerToggle}>
+          <Link to="/">Présentation</Link>
+          <Link to="/conferences">Conférences</Link>
+          <Link to="/planning">Plannification</Link>
+          <Link to="/lives">Diffusions</Link>
+        </nav>
+      </header>
+    );
+  }
+}
 
 export default Header;
