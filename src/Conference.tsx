9df59d7946462db003be01998a18ad4b6b52e8ba
Unknown <elian.hamon@viacesi.fr>
2020-12-11 23:52:15 +0100

    basic style for modals

--- src/Conference.tsx
+++ src/Conference.tsx
@@ -1,5 +1,8 @@
 import React, { ReactElement } from 'react';
 import { ConferenceData, getConferenceRoom } from './data';
+import { Link } from 'react-router-dom';
+
+import cross from './images/cross.svg';
 
 interface ConferenceProps {
   conference: ConferenceData;
@@ -35,12 +38,23 @@ const Conference = (props: ConferenceProps): ReactElement => {
       </article>
 
       <div id={'modal-' + props.conference.id} className="modal">
-        <div className="modal-content">
-          <div className="conference-title">{props.conference.title}</div>
-          <div className="conference-subtitle">{props.conference.subtitle}</div>
-          {props.conference.presenters.join(' - ')}
-          <div className="conference-description">{props.conference.description}</div>
-          <button onClick={() => closeModal('modal-' + props.conference.id)}>close</button>
+        <div className="modal-block">
+          <div className="modal-content">
+            <div className="modal-close">
+              <span className="modal-close-button" onClick={() => closeModal('modal-' + props.conference.id)}><img src={cross} width="20" height="20"></img></span>
+            </div>
+            <div className="conference-title">{props.conference.title}</div>
+            <br />
+            <div className="conference-subtitle">{props.conference.subtitle}</div>
+            <br />
+            {props.conference.presenters.join(' - ')}
+            <br /><br />
+            <div className="conference-description">{props.conference.description}</div>
+            <br />
+            <p>{getConferenceRoom(props.conference).name} - {date.toLocaleDateString()} - {date.toLocaleTimeString()}</p>
+            <br />
+          </div>
+          <Link className="btn" to={'/lives/' + getConferenceRoom(props.conference).id}>VOIR LE LIVE</Link>
         </div>
         <div className="modal-around" onClick={() => closeModal('modal-' + props.conference.id)}></div>
       </div>
