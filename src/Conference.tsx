977fa780604746744a3734d5729636d577b4cf72
Unknown <elian.hamon@viacesi.fr>
2020-12-12 14:20:48 +0100

    modal cleanup

--- src/Conference.tsx
+++ src/Conference.tsx
@@ -1,64 +1,44 @@
 import React, { ReactElement } from 'react';
-import { ConferenceData, getConferenceRoom } from './data';
-import { Link } from 'react-router-dom';
-
-import cross from './images/cross.svg';
+import { ConferenceData, getConferenceRoom, getConference, RoomData } from './data';
 
 interface ConferenceProps {
   conference: ConferenceData;
 }
-const showModal = (name: string) => {
-  document.getElementById(name).style.visibility = 'visible';
-};
 
-const closeModal = (name: string) => {
-  document.getElementById(name).style.visibility = 'hidden';
+const showModal = (id: number) => {
+  const conf: ConferenceData = getConference(id);
+  const room: RoomData = getConferenceRoom(conf);
+  const date: Date = new Date(conf.begin * 1000);
+
+  document.getElementById('modal-title').innerText = conf.title;
+  document.getElementById('modal-subtitle').innerText = conf.subtitle;
+  document.getElementById('modal-presenters').innerText = conf.presenters.join(' - ');
+  document.getElementById('modal-description').innerText = conf.description;
+  document.getElementById('modal-misc').innerText = room.name + ' - ' + date.toLocaleDateString() + ' - ' + date.toLocaleTimeString();
+  document.getElementById('modal-link').setAttribute('href', '#/lives/' + room.id); // TODO
+  document.getElementById('modal').style.visibility = 'visible';
 };
 
 const Conference = (props: ConferenceProps): ReactElement => {
   const date: Date = new Date(props.conference.begin * 1000);
   return (
-    <React.Fragment>
-      <article onClick={() => showModal('modal-' + props.conference.id)}>
-        <div className="left">
-          <div className="roomName">
-            {getConferenceRoom(props.conference).name}
-          </div>
-          <div className="date">
-            {date.toLocaleDateString()}
-            <br />
-            {date.toLocaleTimeString()}
-          </div>
+    <article onClick={() => showModal(props.conference.id)}>
+      <div className="left">
+        <div className="roomName">
+          {getConferenceRoom(props.conference).name}
         </div>
-        <div>
-          <div className="conference-title">{props.conference.title}</div>
-          <div className="conference-subtitle">{props.conference.subtitle}</div>
-          {props.conference.presenters.join(' - ')}
+        <div className="date">
+          {date.toLocaleDateString()}
+          <br />
+          {date.toLocaleTimeString()}
         </div>
-      </article>
-
-      <div id={'modal-' + props.conference.id} className="modal">
-        <div className="modal-block">
-          <div className="modal-content">
-            <div className="modal-close">
-              <span className="modal-close-button" onClick={() => closeModal('modal-' + props.conference.id)}><img src={cross} width="20" height="20"></img></span>
-            </div>
-            <div className="conference-title">{props.conference.title}</div>
-            <br />
-            <div className="conference-subtitle">{props.conference.subtitle}</div>
-            <br />
-            {props.conference.presenters.join(' - ')}
-            <br /><br />
-            <div className="conference-description">{props.conference.description}</div>
-            <br />
-            <p>{getConferenceRoom(props.conference).name} - {date.toLocaleDateString()} - {date.toLocaleTimeString()}</p>
-            <br />
-          </div>
-          <Link className="btn" to={'/lives/' + getConferenceRoom(props.conference).id}>VOIR LE LIVE</Link>
-        </div>
-        <div className="modal-around" onClick={() => closeModal('modal-' + props.conference.id)}></div>
       </div>
-    </React.Fragment>
+      <div>
+        <div className="conference-title">{props.conference.title}</div>
+        <div className="conference-subtitle">{props.conference.subtitle}</div>
+        {props.conference.presenters.join(' - ')}
+      </div>
+    </article>
   );
 };
 
--- src/Conferences.tsx
+++ src/Conferences.tsx
@@ -1,17 +1,44 @@
 import React, { ReactElement } from 'react';
+import { Link } from 'react-router-dom';
 import Conference from './Conference';
 import { ConferenceData } from './data';
 import './Conferences.scss';
+import cross from './images/cross.svg';
 
 interface ConferencesProps {
   conferences: ConferenceData[];
 }
+
+const closeModal = () => {
+  document.getElementById('modal').style.visibility = 'hidden';
+};
 
 const Conferences = (props: ConferencesProps): ReactElement => (
   <React.Fragment>
     {props.conferences.map((conference) => (
       <Conference key={conference.id} {...{ conference }} />
     ))}
+
+    <div id="modal" className="modal">
+      <div className="modal-block">
+        <div className="modal-content">
+          <div className="modal-close">
+            <span className="modal-close-button" onClick={closeModal}><img src={cross} width="20" height="20"></img></span>
+          </div>
+          <div id="modal-title" className="conference-title"></div>
+          <br />
+          <div id="modal-subtitle" className="conference-subtitle"></div>
+          <br />
+          <div id="modal-presenters"></div>
+          <br />
+          <div id="modal-description" className="conference-description"></div>
+          <br />
+          <p id="modal-misc"></p>
+        </div>
+        <Link id="modal-link" className="btn" to="">VOIR LE LIVE</Link>
+      </div>
+      <div className="modal-around" onClick={closeModal}></div>
+    </div>
   </React.Fragment>
 );
 
