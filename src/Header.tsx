e627446a42c6f95f4e49212db86ac0320bf04148
Aurélien <aurelien.pottier@viacesi.fr>
2020-12-12 17:44:50 +0100

    Add first presentation style + scss + bg-image
    Add first presentation style + scss + bg-image

--- src/Header.tsx
+++ src/Header.tsx
@@ -24,7 +24,7 @@ class Header extends Component<unknown, HeaderState> {
   render(): ReactElement {
     return (
       <header>
-        <img src={Logo} alt="Logo GreenTech" />
+        <img className="logo" src={Logo} alt="Logo GreenTech" />
         <div id="burgerToggle" className={this.state.burgerToggle ? 'pressed' : ''} onClick={this.handleClick}>
           <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="34px" height="27px" viewBox="0 0 34 27" enableBackground="new 0 0 34 27">
             <rect fill="#FFFFFF" width="34" height="4" />
--- src/Presentation.tsx
+++ src/Presentation.tsx
@@ -1,8 +1,52 @@
-import React, { ReactElement } from 'react';
+import React, { Component, ReactElement } from 'react';
+import { NavLink } from 'react-router-dom';
+import './Presentation.scss';
+import cameraImage from './images/logo-dark.svg';
+import greenDiscover from './images/logo-dark.svg';
 
 const Presentation = (): ReactElement => (
   <React.Fragment>
+    <div className="container-fluid background-green">
+      <div className="overlay-gray">
+        <h1 className="main-title">GreenTech</h1>
+      </div>
+    </div>
+    <div className="container-fluid discover">
+      <div className="row">
+        <div className="col-6">
+            <img src={cameraImage} className="discover__main-image"/>
+        </div>
+        <div className="col-6">
+            <img src={greenDiscover} className="discover__sec-image"/>
+            <a href="#">
+              <div className="bg-button">
+                  Decouvrez notre programme
+              </div>
+            </a>
+        </div>
+      </div>
+    </div>
+    <div className="container-fluid chronometer">
+      <div className="chronometer__title">
+        Début des conférences dans :
+      </div>
+      <div className="chronometer__value">
+        23 Jours 04 heures 50 min et 12 sec
+      </div>
+
+    </div>
   </React.Fragment>
 );
 
 export default Presentation;
+class Test {
+  render(): ReactElement {
+    return (
+      <div className="container-fluid">
+        Test2
+      </div>
+    );
+  }
+}
+
+
