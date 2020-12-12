90cc8c08938789c80c1abc92d1f2bd8868fdb7e8
Unknown <elian.hamon@viacesi.fr>
2020-12-12 18:50:37 +0100

    add Modal content

--- src/ConferenceModal.tsx
+++ src/ConferenceModal.tsx
@@ -11,27 +11,30 @@ interface ConferenceModalProps {
   setModal: (element: ReactElement) => void;
 }
 
-const ConferenceModal = (props: ConferenceModalProps): ReactElement => (
-  <div id="modal" className="modal">
-    <div className="modal-block">
-      <div className="modal-content">
-        <div className="modal-close">
-          <img src={cross} width="20" height="20" onClick={() => props.setModal(null)}></img>
+const ConferenceModal = (props: ConferenceModalProps): ReactElement => {
+  const date: Date = new Date(props.conference.begin * 1000);
+  return (
+    <div className="modal">
+      <div className="modal-block">
+        <div className="modal-content">
+          <div className="modal-close">
+            <img src={cross} width="20" height="20" onClick={() => props.setModal(null)}></img>
+          </div>
+          <div className="conference-title">{props.conference.title}</div>
+          <br />
+          <div className="conference-subtitle">{props.conference.subtitle}</div>
+          <br />
+          <div>{props.conference.presenters.join(' - ')}</div>
+          <br />
+          <div className="conference-description">{props.conference.description}</div>
+          <br />
+          <p>{getConferenceRoom(props.conference).name} - {date.toLocaleDateString()} - {date.toLocaleTimeString()}</p>
         </div>
-        <div id="modal-title" className="conference-title"></div>
-        <br />
-        <div id="modal-subtitle" className="conference-subtitle"></div>
-        <br />
-        <div id="modal-presenters"></div>
-        <br />
-        <div id="modal-description" className="conference-description"></div>
-        <br />
-        <p id="modal-misc"></p>
+        <Link className="btn" to={'/lives/' + getConferenceRoom(props.conference).id} onClick={() => props.setModal(null)}>VOIR LE LIVE</Link>
       </div>
-      <Link id="modal-link" className="btn" to={'/lives/' + getConferenceRoom(props.conference).id}>VOIR LE LIVE</Link>
+      <div className="modal-around" onClick={() => props.setModal(null)}></div>
     </div>
-    <div className="modal-around" onClick={() => props.setModal(null)}></div>
-  </div>
-);
+  );
+};
 
 export default withModal(ConferenceModal);
