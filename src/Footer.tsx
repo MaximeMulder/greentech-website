4e1fea8c33b3fc2389df21ea638cc2eada124f5a
Maxime Mulder <maxime.mulder@viacesi.fr>
2020-12-13 20:21:54 +0100

    fix footer style

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -6,34 +6,34 @@ import MailLogo from './images/mail.png';
 
 const Footer = (): ReactElement => (
   <footer>
-      <div className="container-fluid main-container">
-        <div className="row">
-          <div className="col-xl-6 col-sm-12 bar">
-            <div>
-              <p>Partenaires</p>
-            </div>
+    <div className="container-fluid main-container">
+      <div className="row">
+        <div className="col-xl-6 col-sm-12 bar">
+          <div>
+            <p>Partenaires</p>
           </div>
-          <div className="col-xl-6 col-sm-12 underbar">
-            <div>
-              <p>Contact</p>
-              <div className="partner container">
-                <div className="row">
-                  <div className="col-4">
-                    <a href="https://twitter.com"><img src={TwitterLogo}/></a>
-                  </div>
-                  <div className="col-4">
-                    <a href="https://linkedin.com"><img src={LinkedinLogo} /></a>
-                  </div>
-                  <div className="col-4">
-                    <a href="mailto:nullepart@mozilla.org"><img src={MailLogo} /></a>
-                  </div>
+        </div>
+        <div className="col-xl-6 col-sm-12 underbar">
+          <div>
+            <p>Contact</p>
+            <div className="partner container">
+              <div className="row">
+                <div className="col-4">
+                  <a href="https://twitter.com"><img src={TwitterLogo} /></a>
+                </div>
+                <div className="col-4">
+                  <a href="https://linkedin.com"><img src={LinkedinLogo} /></a>
+                </div>
+                <div className="col-4">
+                  <a href="mailto:nullepart@mozilla.org"><img src={MailLogo} /></a>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </footer>
+    </div>
+  </footer>
 );
 
 export default Footer;
