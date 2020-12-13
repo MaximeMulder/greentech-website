4dbff5d9f584578bb0c5f45ce20e93995e783d10
Aurélien <aurelien.pottier@viacesi.fr>
2020-12-13 20:18:36 +0100

    Fix Application/Footer

--- src/Application.tsx
+++ src/Application.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement } from 'react';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 
 import Conferences from './Conferences';
+import Footer from './Footer';
 import Header from './Header';
 import Lives from './Lives';
 import Presentation from './Presentation';
@@ -21,6 +22,7 @@ const Application = (): ReactElement => (
           <Route path="/planning" component={Planning} />
           <Route path="/lives" component={Lives} />
         </Switch>
+        <Footer />
       </div>
     </ModalProvider>
   </HashRouter>
