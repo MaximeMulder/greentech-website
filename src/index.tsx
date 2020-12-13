dad840e40d933cf822f6f645615111584d37950b
Maxime Mulder <maxime.mulder@viacesi.fr>
2020-12-13 11:42:27 +0100

    split application component from index

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  HashRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
-
-import Conferences from './Conferences';
-import Header from './Header';
-import Lives from './Lives';
-import Presentation from './Presentation';
-import Planning from './Planning';
-import { ModalProvider } from './Modal';
+import Application from './Application';
 
 import './index.scss';
 
-const App = () => (
-  <Router>
-    <ModalProvider>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Presentation} />
-        <Route path="/conferences" component={Conferences} />
-        <Route path="/planning" component={Planning} />
-        <Route path="/lives" component={Lives} />
-      </Switch>
-    </ModalProvider>
-  </Router>
-);
-
 ReactDOM.render(
-  <App />,
+  <Application />,
   document.getElementById('root')
 );
