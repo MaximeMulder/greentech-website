71e6aab2664c51c474232adb36626ad0436553a5
Maxime Mulder <maxime.mulder@viacesi.fr>
2020-12-14 14:44:36 +0100

    rework footer to remove bootstrap

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,36 +1,28 @@
 import React, { ReactElement } from 'react';
+
+import mail from './images/mail.png';
+import linkedin from './images/linkedin.png';
+import twitter from './images/twitter.svg';
 import './Footer.scss';
-import TwitterLogo from './images/twitter.svg';
-import LinkedinLogo from './images/linkedin.png';
-import MailLogo from './images/mail.png';
 
 const Footer = (): ReactElement => (
-  <footer>
-    <div className="container-fluid main-container">
-      <div className="row">
-        <div className="col-xl-6 col-sm-12 bar">
-          <div>
-            <p>Partenaires</p>
-          </div>
-        </div>
-        <div className="col-xl-6 col-sm-12 underbar">
-          <div>
-            <p>Contact</p>
-            <div className="partner container">
-              <div className="row">
-                <div className="col-4">
-                  <a href="https://twitter.com"><img src={TwitterLogo} /></a>
-                </div>
-                <div className="col-4">
-                  <a href="https://linkedin.com"><img src={LinkedinLogo} /></a>
-                </div>
-                <div className="col-4">
-                  <a href="mailto:nullepart@mozilla.org"><img src={MailLogo} /></a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+  <footer className="footer">
+    <div className="footer-part">
+      <div className="footer-title">Partenaires</div>
+      <div className="footer-list"></div>
+    </div>
+    <div className="footer-part">
+      <div className="footer-title">Contact</div>
+      <div className="footer-list">
+        <a href="https://twitter.com/">
+          <img className="footer-logo" src={twitter} />
+        </a>
+        <a href="https://linkedin.com/">
+          <img className="footer-logo" src={linkedin} />
+        </a>
+        <a href="mailto:none">
+          <img className="footer-logo" src={mail} />
+        </a>
       </div>
     </div>
   </footer>
