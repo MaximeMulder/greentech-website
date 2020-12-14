26be543ec0e3bc6343c536028d0312dd369a657b
Maxime Mulder <maxime.mulder@viacesi.fr>
2020-12-14 16:10:29 +0100

    refactor presentation css

--- src/Presentation.tsx
+++ src/Presentation.tsx
@@ -1,41 +1,24 @@
 import React, { ReactElement } from 'react';
+import { Link } from 'react-router-dom';
 import Timer from './Timer';
 import { getConferences } from './data';
 
-import cameraImage from './images/logo-dark.svg';
-import greenDiscover from './images/logo-dark.svg';
+import logo from './images/logo-dark.svg';
 import './Presentation.scss';
 
 const Presentation = (): ReactElement => (
   <React.Fragment>
-    <div className="background-green">
-      <div className="overlay-gray">
-        <h1 className="main-title">GreenTech</h1>
-        <p>Webinaire sur les thématiques Ville Durable et Industrie 4.0 organisé par des étudiants du CESI Rouen.</p>
-      </div>
+    <div className="presentation-background">
+      <h1 className="presentation-title">GREENTECH</h1>
+      <p className="presentation-paragraph">WEBINAIRE SUR LES THEMATIQUES VILLE DURABLE ET INDUSTRIE 4.0 ORGANISE PAR LES ETUDIANTS DU CESI ROUEN.</p>
     </div>
-    <div className="container-fluid discover">
-      <div className="row discover__row">
-        <div className="col-xl-6 col-sm-12 discover__left mobile-pad">
-          <img src={cameraImage} className="discover__main-image" />
-        </div>
-        <div className="col-xl-6 col-sm-12 discover__right">
-          <img src={greenDiscover} className="discover__sec-image" />
-          <a href="#">
-            <div className="bg-button">
-              Decouvrez notre programme
-            </div>
-          </a>
-        </div>
-      </div>
+    <div className="presentation-program">
+      <img className="presentation-logo" src={logo} />
+      <Link className="presentation-button" to="/planning">DECOUVREZ NOTRE PROGRAMME </Link>
     </div>
-    <div className="container-fluid chronometer">
-      <div className="chronometer__title">
-        Debut des conferences dans :
-      </div>
-      <div className="chronometer__value">
-        <Timer time={getConferences().reduce((a, b) => a.begin < b.begin ? a : b).begin} />
-      </div>
+    <div className="presentation-timer">
+      <div className="presentation-catchphrase">DEBUT DES CONFERENCES DANS:</div>
+      <Timer time={getConferences().reduce((a, b) => a.begin < b.begin ? a : b).begin} />
     </div>
   </React.Fragment>
 );
