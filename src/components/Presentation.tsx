882dad1ec4ad317907e7455bfd56865dd68948fc
Maxime Mulder <maxime.mulder@viacesi.fr>
2021-01-04 21:15:17 +0100

    improve presentation page

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -10,15 +10,15 @@ const Presentation = (): ReactElement => (
   <React.Fragment>
     <div className="presentation-background">
       <h1 className="presentation-title">GREENTECH</h1>
-      <p className="presentation-paragraph">WEBINAIRE SUR LES THEMATIQUES VILLE DURABLE ET INDUSTRIE 4.0 ORGANISE PAR LES ETUDIANTS DU CESI ROUEN.</p>
+      <p className="presentation-paragraph">WEBINAIRE SUR LES THEMATIQUES VILLE DURABLE ET INDUSTRIE 4.0 ORGANISE PAR LES ETUDIANTS DU CESI ROUEN</p>
     </div>
     <div className="presentation-program">
       <img className="presentation-logo" src={logo} />
-      <Link className="presentation-button" to="/planning">DECOUVREZ NOTRE PROGRAMME </Link>
+      <Link className="presentation-button" to="/planning">DECOUVREZ NOTRE PROGRAMME</Link>
     </div>
     <div className="presentation-timer">
       <div className="presentation-catchphrase">DEBUT DES CONFERENCES DANS:</div>
-      <Timer time={getConferences().reduce((a, b) => a.begin < b.begin ? a : b).begin} />
+      <Timer time={getConferences().sort((a, b) => a.begin - b.begin)[0].begin} />
     </div>
   </React.Fragment>
 );
