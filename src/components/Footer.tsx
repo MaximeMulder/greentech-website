6f1ff5d9ab182fde8ec0d0e90da6ea7f10cef165
Maxime Mulder <maxime.mulder@viacesi.fr>
2021-01-07 01:01:02 +0100

    remove partners from footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,23 +7,17 @@ import './Footer.scss';
 
 const Footer = (): ReactElement => (
   <footer className="footer">
-    <div className="footer-part">
-      <div className="footer-title">Partenaires</div>
-      <div className="footer-list"></div>
-    </div>
-    <div className="footer-part">
-      <div className="footer-title">Contact</div>
-      <div className="footer-list">
-        <a href="https://twitter.com/greentechrouen">
-          <img className="footer-logo" src={twitter} />
-        </a>
-        <a href="https://www.linkedin.com/company/greentech-rouen/">
-          <img className="footer-logo" src={linkedin} />
-        </a>
-        <a href="mailto:greentech.rouen@gmail.com">
-          <img className="footer-logo" src={mail} />
-        </a>
-      </div>
+    <div className="footer-title">Contact</div>
+    <div className="footer-list">
+      <a href="https://twitter.com/greentechrouen">
+        <img className="footer-logo" src={twitter} />
+      </a>
+      <a href="https://www.linkedin.com/company/greentech-rouen/">
+        <img className="footer-logo" src={linkedin} />
+      </a>
+      <a href="mailto:greentech.rouen@gmail.com">
+        <img className="footer-logo" src={mail} />
+      </a>
     </div>
   </footer>
 );
