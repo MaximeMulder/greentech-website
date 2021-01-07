7770cfa4e6a7ba24c3551c44020480e73767bcdc
Nenrikido <ampy00@live.fr>
2021-01-07 01:41:03 +0100

    add preview of next conference + fix timer

--- src/components/Live.tsx
+++ src/components/Live.tsx
@@ -14,19 +14,25 @@ function Live(): ReactElement {
   const conferences = getRoomConferences(room);
   const current_time = (new Date()).getTime() / 1000;
   const current_conference: ConferenceData = conferences.find((a) => (a.begin <= current_time && current_time <= a.end));
-  return (
-    <div className="live">
-      <iframe className="live-video" src={current_conference ? current_conference.url : room.live} title={room.name} />
-      {current_conference ? (
+  if (current_conference) {
+    return (
+      <div className="live">
+        <iframe className="live-video" src={current_conference.url} title={current_conference.title} />
         <div className="live-conf-title">Conference en cours : <br />{current_conference.title}</div>
-      ) : (
+      </div>
+    );
+  } else {
+    const next_conference = conferences.filter((a) => a.begin > current_time).reduce((a, b) => a.begin < b.begin ? a : b);
+    return (
+      <div className="live">
+        <iframe className="live-video" src={next_conference.url} title={next_conference.title} />
         <div className="live-timer">
-          <div className="live-catchphrase">LA DIFFUSION DEBUTE DANS :</div>
-          <Timer time={conferences.reduce((a, b) => a.begin < b.begin ? a : b).begin} />
+          <div className="live-catchphrase">La prochaine conference debute dans :</div>
+          <Timer time={next_conference.begin} />
         </div>
-      )}
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default Live;
