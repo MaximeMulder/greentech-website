a47c9c81aa020b2f25061e0fd5bed4001f9f66ec
Maxime Mulder <maxime.mulder@viacesi.fr>
2021-01-07 21:52:12 +0100

    add partners to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from 'react';
 
+import cesi from '../images/cesi.svg';
 import mail from '../images/mail.svg';
 import linkedin from '../images/linkedin.svg';
 import twitter from '../images/twitter.svg';
@@ -7,17 +8,25 @@ import './Footer.scss';
 
 const Footer = (): ReactElement => (
   <footer className="footer">
-    <div className="footer-title">Contact</div>
-    <div className="footer-list">
-      <a href="https://twitter.com/greentechrouen">
-        <img className="footer-logo" src={twitter} />
-      </a>
-      <a href="https://www.linkedin.com/company/greentech-rouen/">
-        <img className="footer-logo" src={linkedin} />
-      </a>
-      <a href="mailto:greentech.rouen@gmail.com">
-        <img className="footer-logo" src={mail} />
-      </a>
+    <div className="footer-part">
+      <div className="footer-title">Partenaires</div>
+      <div className="footer-list">
+        <img className="footer-image" src={cesi} />
+      </div>
+    </div>
+    <div className="footer-part">
+      <div className="footer-title">Contact</div>
+      <div className="footer-list">
+        <a href="https://twitter.com/greentechrouen" target="_blank" rel="noreferrer">
+          <img className="footer-image" src={twitter} />
+        </a>
+        <a href="https://www.linkedin.com/company/greentech-rouen/" target="_blank" rel="noreferrer">
+          <img className="footer-image" src={linkedin} />
+        </a>
+        <a href="mailto:greentech.rouen@gmail.com" target="_blank" rel="noreferrer">
+          <img className="footer-image" src={mail} />
+        </a>
+      </div>
     </div>
   </footer>
 );
