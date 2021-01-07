88f5dc5f3357cceae9f46d29999246f78196cff2
Maxime Mulder <maxime.mulder@viacesi.fr>
2021-01-07 08:09:12 +0100

    improve lives page

--- src/components/Live.tsx
+++ src/components/Live.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from 'react';
 import { useParams } from 'react-router';
 import Timer from './Timer';
-import { getRoomConferences, getRoom, ConferenceData } from '../data';
+import { getRoomConferences, getRoom } from '../data';
 
 import './Live.scss';
 
@@ -12,27 +12,39 @@ interface LiveParams {
 function Live(): ReactElement {
   const room = getRoom(Number(useParams<LiveParams>().live));
   const conferences = getRoomConferences(room);
-  const current_time = (new Date()).getTime() / 1000;
-  const current_conference: ConferenceData = conferences.find((a) => (a.begin <= current_time && current_time <= a.end));
-  if (current_conference) {
-    return (
-      <div className="live">
-        <iframe className="live-video" src={current_conference.url} title={current_conference.title} />
-        <div className="live-conf-title">Conference en cours : <br />{current_conference.title}</div>
-      </div>
-    );
-  } else {
-    const next_conference = conferences.filter((a) => a.begin > current_time).reduce((a, b) => a.begin < b.begin ? a : b);
-    return (
-      <div className="live">
-        <iframe className="live-video" src={next_conference.url} title={next_conference.title} />
-        <div className="live-timer">
-          <div className="live-catchphrase">La prochaine conference debute dans :</div>
-          <Timer time={next_conference.begin} />
-        </div>
-      </div>
-    );
+  const time = Date.now() / 1000;
+  let conference = conferences.find((conference) => time >= conference.begin && time <= conference.end);
+  if (!conference) {
+    const nexts = conferences.filter((conference) => time < conference.begin);
+    if (nexts.length > 0) {
+      conference = nexts.reduce((a, b) => a.begin < b.begin ? a : b);
+    }
   }
+
+  if (!conference) {
+    conference = conferences.reduce((a, b) => a.begin > b.begin ? a : b);
+  }
+
+  return (
+    <div className="live">
+      <iframe className="live-video" src={conference.url} title={conference.title} />
+      <div className="live-description">
+        {time < conference.begin ? (
+          'LA PROCHAINE CONFERENCE DEBUTE DANS :'
+        ) : time > conference.end ? (
+          'DERNIERE CONFERENCE :'
+        ) : (
+          'CONFERENCE EN COURS :'
+        )}
+        <br />
+        {time > conference.begin ? (
+          conference.title
+        ) : (
+          <Timer time={conference.begin} />
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Live;
--- src/data.ts
+++ src/data.ts
@@ -1,7 +1,6 @@
 export interface RoomData {
   id: number;
   name: string;
-  live: string;
 }
 
 export interface ConferenceData {
@@ -9,7 +8,6 @@ export interface ConferenceData {
   title: string;
   subtitle: string;
   description: string;
-  image: string;
   presenters: string[];
   room: number;
   url: string;
@@ -23,7 +21,6 @@ const conferences = [
     title: 'Pollution numérique',
     subtitle: 'Découvrez comment améliorer votre empreinte numérique.',
     description: 'Présenter ce qu\'est la pollution numérique, les causes et ce qui contribue à la réduire.',
-    image: 'image url',
     presenters: [
       'Adrien Dossang',
       'Arthur Delamare',
@@ -40,7 +37,6 @@ const conferences = [
     title: 'Cloud computing',
     subtitle: 'L\'utilisation du Cloud Computing dans la gestion de la ville.',
     description: 'L\'évolution continuelle des technologies numériques nous montre au quotidien que la ville de demain sera en grande partie, si ce n\'est entièrement, connectée. De nouveaux services numériques voient le jour, tandis que d\'autres bénéficient des avancées technologiques permettant d\'améliorer la qualité des services actuels. Les différentes applications du cloud computing illustrent parfaitement cet exemple. En effet, certains services du cloud d\'aujourd\'hui évoluent tandis que d\'autres essayent de faire leurs preuves comme c\'est le cas pour le cloud gaming. Notre objectif de recherche sera d\'étudier et tenter de prévoir l\'évolution de ces technologies. Nos travaux porteront donc sur le développement des technologies du cloud, leur adoption ainsi que les nouvelles branches du cloud qui selon nous pourraient émerger ces prochaines années dans le cas d\'une ville durable et/ou de l\'industrie 4.0.',
-    image: 'image url',
     presenters: [
       'Antoine Savalle',
       'Maxime Mulder',
@@ -57,7 +53,6 @@ const conferences = [
     title: 'Cybersécurité des smart city',
     subtitle: 'Les enjeux de sécurité impliqués par l\'évolution des technologies dans les villes.',
     description: 'Nous allons parler de la cybersécurité des Smart Cities d\'hier et d\'aujourd\'hui et allons donner nos prévisions pour les 5 années à venir.',
-    image: 'image url',
     presenters: [
       'Julien Zolli',
       'Nicolas Van-hove',
@@ -72,7 +67,6 @@ const conferences = [
     title: 'Fluidifier le trafic',
     subtitle: 'L\'utilisation des nouvelles technologies pour optimiser le trafic.',
     description: 'La gestion des embouteillages est l\'un des enjeux principaux au sein des smart cities car il a un impact sur la réduction de la pollution atmosphérique et sonore. L\'IA et le Machine Learning peuvent être utilisés pour fluidifier le trafic, l\'IA accumule de nombreuses données sur le trafic puis les analyse afin de prendre une décision pour améliorer le trafic. Par exemple, l\'IA peut détecter des embouteillages sur une route et peut décider de dévier le trafic grâce aux feux de circulations.',
-    image: 'image url',
     presenters: [
       'Kévin Huard',
       'Corentin Dragée',
@@ -87,7 +81,6 @@ const conferences = [
     title: 'Télécommunications',
     subtitle: 'Permettre un accès continu à internet dans toute la métropole.',
     description: 'Dans cette conférence nous aborderons le sujet de l\'accès à internet dans nos villes de demain. Premièrement, nous présenterons des problèmes soulevés par le manque d\'accès à internet en ville, puis des exemples de cas d\'utilisation possible grâce à la mise en place d\'un accès à internet dans la métropole. Ensuite, nous parlerons des technologies innovantes en rapport notre problématique. Pour conclure nous présenterons une étude de faisabilité de ce projet à l\'échelle d\'une métropole.',
-    image: 'image url',
     presenters: [
       'Victor Personnettaz',
       'Yanis Mercier',
@@ -102,7 +95,6 @@ const conferences = [
     title: 'Énergies',
     subtitle: 'CO² et réchauffement climatique : au pire on mettra des tee-shirts.',
     description: 'Conférence sur le thème du réchauffement climatique qui aura pour but de vous présenter quelques points intéressants du dernier rapport du GIEC!',
-    image: 'image url',
     presenters: [
       'Paul Fontaine',
     ],
@@ -116,7 +108,6 @@ const conferences = [
     title: 'Liberté des citoyens',
     subtitle: 'Problèmes et protections de la vie privée dans la Smart City.',
     description: 'Nous étudierons la place de la vie privée dans la smart city. Quels sont les problèmes liés ? Quelles sont les lois qui protègent la vie prive aujourd\'hui ? Etude de cas de la Chine. Les solutions que nous proposons.',
-    image: 'image url',
     presenters: [
       'Etienne Jouvençon',
       'Jean-Marc Meschin',
@@ -133,7 +124,6 @@ const conferences = [
     title: 'Fermes urbaines',
     subtitle: 'Présentation des fermes urbaines et de ses enjeux et innovations.',
     description: 'Notre conférence présentera le sujet des fermes urbaines, traitera de leur état ainsi que leur place dans la smart city et industrie 4.0. Nous commencerons sur un état de l\'art suivi de pistes d\'améliorations et les innovations.',
-    image: 'image url',
     presenters: [
       'Baptiste Pimont',
       'Bryan Quatremare',
@@ -149,7 +139,6 @@ const conferences = [
     title: 'GreenIT au sein de la DSI',
     subtitle: 'Présentation du concept et de ses bonnes pratiques.',
     description: 'Introduction au concept de l\'informatique durable. Présentation de bonnes pratiques en matière d\'informatique durable (matériel, hébergement, développement).',
-    image: 'image url',
     presenters: [
       'Alexandre Dassonville',
       'Antonin Paul',
@@ -164,7 +153,6 @@ const conferences = [
     title: 'Voitures autonomes',
     subtitle: 'Les impacts de la voiture autonome sur la société de demain.',
     description: 'Partant du postulat que la voiture autonome fera partie du champ de l\'IOT (Internet of Things) et que l\'évolution de la MaaS (Mobility as a Service) mettra fin au rapport que nous avons avec nos voitures individuelles, nous sommes en droit de nous demander quels en seront l\'impact sur notre société et nos infrastructures ?',
-    image: 'image url',
     presenters: [
       'Antoine Morlet',
     ],
@@ -178,7 +166,6 @@ const conferences = [
     title: 'Gestion des flux',
     subtitle: '',
     description: 'Voitures, piétons, transports en commun, services publics, électricité, eau : De nombreux flux parcourent nos villes en tout temps. Quelles sont les relations que la Smart City entretient avec ces flux ? Et surtout, par quels moyens peut-elle les fluidifier, aujourd\'hui et dans les années à l\'avenir ? Nous allons nous focaliser sur les flux de personnes dans cette conférence et nous nous efforcerons de répondre à ces questions sur l\'heure qui nous a été allouée.',
-    image: 'image url',
     presenters: [
       'Maximilien Fonteyn',
       'Virgile Marand',
@@ -194,7 +181,6 @@ const conferences = [
     title: 'Smart harbor',
     subtitle: 'L\'utilisation des technologies numériques pour la gestion des ports maritimes.',
     description: 'Avec une augmentation constante du trafic maritime, les technologies numériques se sont imposées comme des solutions incontournables auprès des ports pour améliorer leurs compétitivités tout en respectant l\'environnement.',
-    image: 'image url',
     presenters: [
       'Gabriel Lesourd',
     ],
@@ -208,7 +194,6 @@ const conferences = [
     title: 'Portefeuille numérique',
     subtitle: '',
     description: 'De nos jours, nos papiers personnels sont très importants dans notre vie quotidienne et surtout avec le confinement, la situation va être de plus en plus compliquée puisqu\'à chaque sortie, on doit être muni d\'une attestation de déplacement et un justificatif d\'identité. Notre solution est d\'informatiser toutes les cartes personnelles comme la carte d\'identité et la carte bancaire et réaliser un profil numérique qui contient toutes les coordonnés personnelles pour éliminer les cartes tangibles, alors on va réaliser un système de contrôle d\'accès biométrique qui se base sur la reconnaissance de l\'iris liée avec un système de reconnaissance faciale en 3D qui est liée directement avec le cloud. On va appliquer cette solution pour faire des paiements sans besoin d\'apporter une carte bancaire et même sans besoin de retenir des mots de passe, notre solution est un moyen pour faciliter le paiement n\'importe où (autoroute, caisse en magasin, pharmacie, etc….).',
-    image: 'image url',
     presenters: [
       'Alaeddine Abdelkafi',
       'Amal Ayadi',
@@ -224,7 +209,6 @@ const conferences = [
     title: 'Propreté et durabilité',
     subtitle: 'La gestion des déchets dans les villes.',
     description: 'Nous parlerons de la gestion intelligente des déchets au sein des smart cities, des technologies existantes, l\'évolution et l\'historique de celles-ci.',
-    image: 'image url',
     presenters: [
       'Nils Montier',
     ],
@@ -238,7 +222,6 @@ const conferences = [
     title: 'Smart buildings',
     subtitle: 'Les enjeux et les risques des Smart Buildings au sein de la Smart City.',
     description: 'Les Smart Buildings seront la base pour une transition vers les Smarts Cities. Il est donc important de comprendre les enjeux, autant écologiques qu\'économiques, des bâtiments des villes de demain, mais aussi les risques qui peuvent être engendrés par un développement de ces smart buildings.',
-    image: 'image url',
     presenters: [
       'Celia Henry',
     ],
@@ -252,7 +235,6 @@ const conferences = [
     title: 'La mobility-as-a-service',
     subtitle: 'Découvrir le concept de mobility-as-a-service et ses implémentations.',
     description: 'Lors de notre conférence vous allez découvrir le sujet de la Mobility-as-a-service et son implémentation à l\'aide d\'exemples concrets ainsi que les futurs infrastructures et usages.',
-    image: 'image url',
     presenters: [
       'Aurélien Pottier',
       'Clément Lesage',
@@ -267,18 +249,15 @@ const conferences = [
 const rooms = [
   {
     id: 0,
-    name: 'SALLE 1',
-    live: 'https://www.youtube.com/embed/0000000000'
+    name: 'SALLE 1'
   },
   {
     id: 1,
-    name: 'SALLE 2',
-    live: 'https://www.youtube.com/embed/0000000000'
+    name: 'SALLE 2'
   },
   {
     id: 2,
-    name: 'SALLE 3',
-    live: 'https://www.youtube.com/embed/0000000000'
+    name: 'SALLE 3'
   }
 ];
 
